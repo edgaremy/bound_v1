166ea611f31fbfd50bf6f10ae78a7dc5b30f235d
edgaremy <remy.edgar@gmail.com>
2024-02-01 11:20:40 +0100

    Also counts number of images with labels

--- get_number_of_labels.py
+++ get_number_of_labels.py
@@ -1,7 +1,8 @@
 import os
 
-def count_lines_in_txt_files(folder_path):
+def count_lines_and_files_in_txt_files(folder_path):
     total_lines = 0
+    total_txt = 0
 
     for root, dirs, files in os.walk(folder_path):
         for file in files:
@@ -9,10 +10,12 @@ def count_lines_in_txt_files(folder_path):
                 file_path = os.path.join(root, file)
                 with open(file_path, "r") as f:
                     total_lines += len(f.readlines())
+                total_txt += 1
 
-    return total_lines
+    return total_lines, total_txt
 
 # Example usage:
 folder_path = "/mnt/disk1/datasets/Lepinoc_2022/splitted_dataset/Task_Lepinoc/labels"
-total_lines = count_lines_in_txt_files(folder_path)
+total_lines, total_txt = count_lines_and_files_in_txt_files(folder_path)
 print(f"Total number of lines in txt files: {total_lines}")
+print(f"Total number of annotated images: {total_txt}")
