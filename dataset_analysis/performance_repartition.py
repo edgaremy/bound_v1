4165c143f9883b32ccba04d6369c71d9f3f9ad59
edgaremy <remy.edgar@gmail.com>
2024-03-15 16:48:20 +0100

    Completed performance graph scripts

--- dataset_analysis/performance_repartition.py
+++ dataset_analysis/performance_repartition.py
@@ -1,12 +1,35 @@
 import os
 import matplotlib.pyplot as plt
 import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
 from ultralytics import YOLO
 import numpy as np
 import os
 
 import get_hierarchy as hierarchy
+import sub_confusion_matrix as scm
+
+def make_ordered_unique(list_):
+    output = []
+    indexes = []
+    for i in range(len(list_)):
+        if list_[i] not in output:
+            output.append(list_[i])
+            indexes.append(i)
+    return np.array(output), np.array(indexes)
+
+def get_upper_hierarchy_index(current_hierarchy, upper_hierarchies):
+    # Associate each unique upper_hierarchy to an index
+    upper_hierarchy_unique, upper_hierarchy_indexes = make_ordered_unique(upper_hierarchies)
+    upper_indexes = []
+    # For each current, get the index of the upper_hierarchy
+    for i in range(len(current_hierarchy)):
+        upper_name = upper_hierarchies[i]
+        upper_index = np.where(upper_hierarchy_unique == upper_name)[0]
+        upper_indexes.append(upper_index[0])
+    return np.array(upper_indexes)
+        
 
 def plot_class_hierarchy_performance(dataset_folder, model_path):
     # Load a model
@@ -53,45 +76,88 @@ def plot_class_hierarchy_performance(dataset_folder, model_path):
                 class_hierarchies[class_folder].append(class_counts[class_folder])
             else:
                 class_hierarchies[class_folder][5] = class_counts[class_folder]
-        
-    # Counting true positives, false positives, true negatives, and false negatives
-    true_positives = np.diag(conf_mat)
-    false_positives = np.sum(conf_mat, axis=1) - true_positives
-    false_negatives = np.sum(conf_mat, axis=0) - true_positives
-    true_negatives = np.sum(conf_mat) - true_positives - false_positives - false_negatives
-    # F1 Score
-    f1_score = 2 * true_positives / (2 * true_positives + false_positives + false_negatives)
-
-    # Keep only remaining classes:
-    true_positives = true_positives[remaining_classes]
-    false_positives = false_positives[remaining_classes]
-    false_negatives = false_negatives[remaining_classes]
-    true_negatives = true_negatives[remaining_classes]
-    f1_score = f1_score[remaining_classes]
-
-    print('Global F1 Score: ', np.mean(f1_score))
 
 
-    # PLOT graph of results
-    data = {
-        'class_': [class_hierarchies[class_folder][0] for class_folder in class_counts.keys()],
-        'order': [class_hierarchies[class_folder][1] for class_folder in class_counts.keys()],
-        'family': [class_hierarchies[class_folder][2] for class_folder in class_counts.keys()],
-        'genus': [class_hierarchies[class_folder][3] for class_folder in class_counts.keys()],
-        'species': [class_hierarchies[class_folder][4] for class_folder in class_counts.keys()],
-        'count': [class_hierarchies[class_folder][5] for class_folder in class_counts.keys()],
-        'F1 Score': f1_score
-    }
-        
+    class_ = np.array([class_hierarchies[class_folder][0] for class_folder in class_counts.keys()])
+    order = np.array([class_hierarchies[class_folder][1] for class_folder in class_counts.keys()])
+    family = np.array([class_hierarchies[class_folder][2] for class_folder in class_counts.keys()])
+    genus = np.array([class_hierarchies[class_folder][3] for class_folder in class_counts.keys()])
+    species = np.array([class_hierarchies[class_folder][4] for class_folder in class_counts.keys()])
+    count = np.array([class_hierarchies[class_folder][5] for class_folder in class_counts.keys()])
 
-    df_ = pd.DataFrame(data)
+    class_unique, class_indexes = make_ordered_unique(class_)
+    order_unique, order_indexes = make_ordered_unique(order)
+    family_unique, family_indexes = make_ordered_unique(family)
+    genus_unique, genus_indexes = make_ordered_unique(genus)
 
-    df_.fillna('unknown', inplace=True)
+    # Per Species F1 Score:
+    f1_score, _, _, _, _ = scm.confusion_matrix_stats(conf_mat)
+    f1_score_species = f1_score[remaining_classes] # Keep only remaining classes
+    print('Species F1 Score: ', np.mean(f1_score_species))
 
-    # Création du diagramme en treillis
-    fig = px.sunburst(df_, path=['class_', 'order', 'family', 'genus', 'species'], color='F1 Score', values='count')
+    # Per Genus F1 Score:
+    species_genus_idx = get_upper_hierarchy_index(species, genus)
+    conf_mat_genus = scm.merge_confusion_matrix(conf_mat, species_genus_idx)
+    f1_score, _, _, _, _ = scm.confusion_matrix_stats(conf_mat_genus)
+    f1_score_genus = f1_score[:] # Keep only remaining classes
+    counts_genus = np.zeros(len(genus_unique))
+    for i in range(len(species_genus_idx)):
+        counts_genus[species_genus_idx[i]] += count[i]
+    print('Genus F1 Score: ', np.mean(f1_score_genus))
 
-    # Affichage du graphique
+    # Per Family F1 Score:
+    species_family_idx = get_upper_hierarchy_index(species, family)
+    conf_mat_family = scm.merge_confusion_matrix(conf_mat, species_family_idx)
+    f1_score, _, _, _, _ = scm.confusion_matrix_stats(conf_mat_family)
+    f1_score_family = f1_score[:] # Keep only remaining classes
+    counts_family = np.zeros(len(family_unique))
+    for i in range(len(species_family_idx)):
+        counts_family[species_family_idx[i]] += count[i]
+    print('Family F1 Score: ', np.mean(f1_score_family))
+
+    # Counting for remaining order and class_:
+    species_order_idx = get_upper_hierarchy_index(species, order)
+    counts_order = np.zeros(len(order_unique))
+    for i in range(len(species_order_idx)):
+        counts_order[species_order_idx[i]] += count[i]
+
+    species_class_idx = get_upper_hierarchy_index(species, class_)
+    counts_class = np.zeros(len(class_unique))
+    for i in range(len(species_class_idx)):
+        counts_class[species_class_idx[i]] += count[i]
+
+    data = dict(
+        names = np.concatenate((class_unique,order_unique,family_unique,genus_unique,species), axis=None),
+        parents = np.concatenate((np.array([''] * len(class_unique)), class_[order_indexes], order[family_indexes], family[genus_indexes], genus), axis=None),
+        values = np.concatenate((counts_class, counts_order, counts_family, counts_genus, count), axis=None),
+        f1_scores = np.concatenate((np.array([1] * len(class_unique)), np.array([1] * len(order_unique)), f1_score_family, f1_score_genus, f1_score_species), axis=None)
+    )
+
+# ['aggrnyl', 'agsunset', 'algae', 'amp', 'armyrose', 'balance',
+#              'blackbody', 'bluered', 'blues', 'blugrn', 'bluyl', 'brbg',
+#              'brwnyl', 'bugn', 'bupu', 'burg', 'burgyl', 'cividis', 'curl',
+#              'darkmint', 'deep', 'delta', 'dense', 'earth', 'edge', 'electric',
+#              'emrld', 'fall', 'geyser', 'gnbu', 'gray', 'greens', 'greys',
+#              'haline', 'hot', 'hsv', 'ice', 'icefire', 'inferno', 'jet',
+#              'magenta', 'magma', 'matter', 'mint', 'mrybm', 'mygbm', 'oranges',
+#              'orrd', 'oryel', 'oxy', 'peach', 'phase', 'picnic', 'pinkyl',
+#              'piyg', 'plasma', 'plotly3', 'portland', 'prgn', 'pubu', 'pubugn',
+#              'puor', 'purd', 'purp', 'purples', 'purpor', 'rainbow', 'rdbu',
+#              'rdgy', 'rdpu', 'rdylbu', 'rdylgn', 'redor', 'reds', 'solar',
+#              'spectral', 'speed', 'sunset', 'sunsetdark', 'teal', 'tealgrn',
+#              'tealrose', 'tempo', 'temps', 'thermal', 'tropic', 'turbid',
+#              'turbo', 'twilight', 'viridis', 'ylgn', 'ylgnbu', 'ylorbr',
+#              'ylorrd']
+
+    fig = px.sunburst(data,
+                      names = 'names',
+                      parents = 'parents',
+                      values = 'values',
+                      color = 'f1_scores',
+                      branchvalues = 'total',
+                      color_continuous_scale='fall_r')
+    # fig.update_layout(uniformtext=dict(minsize=13, mode='hide'))
+    # Affichage du graphique 
     fig.show()
     fig.write_html("./export.html")
     # fig.write_image("hierarchie_especes.svg")
--- dataset_analysis/sub_confusion_matrix.py
+++ dataset_analysis/sub_confusion_matrix.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+# mother_classes[i]=j means that the class i is a subclass of the mother-class j
 def merge_confusion_matrix(confusion_matrix, mother_classes):
     num_classes = len(mother_classes)
     num_mother_classes = max(mother_classes) + 1
@@ -15,10 +16,20 @@ def merge_confusion_matrix(confusion_matrix, mother_classes):
     return merged_matrix
 
 # Example Usage:
-confusion_matrix = np.array([[13, 1, 2, 0],
-                             [0, 10, 0, 0],
-                             [0, 0, 8, 0],
-                             [0, 0, 0, 9]])
-mother_classes = [0, 1, 0, 1]
-merged_matrix = merge_confusion_matrix(confusion_matrix, mother_classes)
-print(merged_matrix)
+# confusion_matrix = np.array([[13, 1, 2, 0],
+#                              [0, 10, 0, 0],
+#                              [0, 0, 8, 0],
+#                              [0, 0, 0, 9]])
+# mother_classes = [0, 1, 0, 1]
+# merged_matrix = merge_confusion_matrix(confusion_matrix, mother_classes)
+# print(merged_matrix)
+
+def confusion_matrix_stats(conf_mat):
+    # Counting true positives, false positives, true negatives, and false negatives
+    true_positives = np.diag(conf_mat)
+    false_positives = np.sum(conf_mat, axis=1) - true_positives
+    false_negatives = np.sum(conf_mat, axis=0) - true_positives
+    true_negatives = np.sum(conf_mat) - true_positives - false_positives - false_negatives
+    # F1 Score
+    f1_score = np.where(true_positives > 0, 2 * true_positives / (2 * true_positives + false_positives + false_negatives), 0)
+    return f1_score, true_positives, false_positives, false_negatives, true_negatives
