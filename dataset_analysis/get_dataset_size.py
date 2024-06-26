824c4996ae7bcf760f491a4faa987bd1f9e9421a
edgaremy <remy.edgar@gmail.com>
2024-06-26 12:14:18 +0200

    Added per class count in dataset analysis

--- dataset_analysis/get_dataset_size.py
+++ dataset_analysis/get_dataset_size.py
@@ -26,28 +26,70 @@ def get_dataset_size(dataset_folder, one_class_only=False):
     print('Number of val images:', num_val_images)
     print('Total number of images:', total_images)
 
-# Example Usage:
-print("Dataset Bee BD307:")
-dataset_folder = "/mnt/disk1/datasets/Projet_Bees_Detection_Basile/data_bees_detection/BD307/BD_307_cropped/dataset"
-get_dataset_size(dataset_folder)
+def get_classes_size(dataset_folder):
+    train_folder = os.path.join(dataset_folder, 'train')
+    test_folder = os.path.join(dataset_folder, 'test')
+    val_folder = os.path.join(dataset_folder, 'val')
 
-print("\nDataset Bee BD71:")
-dataset_folder = "/mnt/disk1/datasets/Projet_Bees_Detection_Basile/data_bees_detection/BD_71_visited/images"
-get_dataset_size(dataset_folder, one_class_only=True)
+    # Get the number of classes
+    classes = os.listdir(train_folder)
+    num_classes = len(classes)
+    print('Number of classes:', num_classes)
 
-print("\nDataset Bee BD71 Cropped:")
-dataset_folder = "/mnt/disk1/datasets/Projet_Bees_Detection_Basile/data_bees_detection/Cropped_BD_71_True/images"
-get_dataset_size(dataset_folder)
+    # Get the number of images in train, test, and val folders
+    num_train_images = sum(len(files) for _, _, files in os.walk(train_folder))
+    num_test_images = sum(len(files) for _, _, files in os.walk(test_folder))
+    num_val_images = sum(len(files) for _, _, files in os.walk(val_folder))
 
-print("\nDataset Bee BD1")
-dataset_folder = "/mnt/disk1/datasets/Projet_Bees_Detection_Basile/data_bees_detection/BD_1class/images"
-get_dataset_size(dataset_folder, one_class_only=True)
+    # Calculate the total number of images
+    total_images = num_train_images + num_test_images + num_val_images
 
-print("\nDataset Arthropods LIMIT6")
-dataset_folder = "/mnt/disk1/datasets/iNaturalist/Arthropods/LIMIT6/dataset/images"
-get_dataset_size(dataset_folder, one_class_only=True)
+    print('Number of train images:', num_train_images)
+    print('Number of test images:', num_test_images)
+    print('Number of val images:', num_val_images)
+    print('Total number of images:', total_images)
 
-print("\nDataset Lepinoc")
-dataset_folder = "/mnt/disk1/datasets/Lepinoc_2022/splitted_dataset/Task_Lepinoc/images"
-get_dataset_size(dataset_folder, one_class_only=True)
+    class_counts = {}
+    for class_folder in os.listdir(train_folder):
+        class_path = os.path.join(train_folder, class_folder)
+        class_counts[class_folder] = len(os.listdir(class_path))
+
+    return class_counts
+
+# Save class count as csv
+def save_class_count_as_csv(dataset_folder, csv_file):
+    class_counts = get_classes_size(dataset_folder)
+    with open(csv_file, 'w') as f:
+        for key in sorted(class_counts.keys()):
+            f.write("%s,%s\n" % (key, class_counts[key]))
+
+# Save class count as csv example:
+# dataset_folder = "/mnt/disk1/datasets/Projet_Bees_Detection_Basile/data_bees_detection/BD307/BD_307_cropped/dataset"
+# csv_file = "./class_counts.csv"
+# save_class_count_as_csv(dataset_folder, csv_file)
+
+# Dataset size Example Usage:
+# print("Dataset Bee BD307:")
+# dataset_folder = "/mnt/disk1/datasets/Projet_Bees_Detection_Basile/data_bees_detection/BD307/BD_307_cropped/dataset"
+# get_dataset_size(dataset_folder)
+
+# print("\nDataset Bee BD71:")
+# dataset_folder = "/mnt/disk1/datasets/Projet_Bees_Detection_Basile/data_bees_detection/BD_71_visited/images"
+# get_dataset_size(dataset_folder, one_class_only=True)
+
+# print("\nDataset Bee BD71 Cropped:")
+# dataset_folder = "/mnt/disk1/datasets/Projet_Bees_Detection_Basile/data_bees_detection/Cropped_BD_71_True/images"
+# get_dataset_size(dataset_folder)
+
+# print("\nDataset Bee BD1")
+# dataset_folder = "/mnt/disk1/datasets/Projet_Bees_Detection_Basile/data_bees_detection/BD_1class/images"
+# get_dataset_size(dataset_folder, one_class_only=True)
+
+# print("\nDataset Arthropods LIMIT6")
+# dataset_folder = "/mnt/disk1/datasets/iNaturalist/Arthropods/LIMIT9/dataset/images"
+# get_dataset_size(dataset_folder, one_class_only=True)
+
+# print("\nDataset Lepinoc")
+# dataset_folder = "/mnt/disk1/datasets/Lepinoc_2022/splitted_dataset/Task_Lepinoc/images"
+# get_dataset_size(dataset_folder, one_class_only=True)
 
