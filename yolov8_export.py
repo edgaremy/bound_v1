8ecf67f24d0ead7dcc340789c8c37897d093bc55
edgaremy <remy.edgar@gmail.com>
2024-09-09 14:55:08 +0200

    Updated to export in tflite

--- yolov8_export.py
+++ yolov8_export.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO('runs/detect/train16/weights/best.pt')
+model = YOLO('runs/detect/train38/weights/best.pt')
 
 # Export the model
-path = model.export(format="onnx")
+path = model.export(format="tflite", int8=True)
