3ee845c9ff22d30c041576b62d3aac1fa965a083
edgaremy <remy.edgar@gmail.com>
2024-10-17 15:42:29 +0200

    Updated export to include onnx format

--- yolov8_export.py
+++ yolov8_export.py
@@ -1,7 +1,10 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO('runs/detect/train38/weights/best.pt')
+model = YOLO('runs/detect/train40/weights/best.pt')
 
 # Export the model
-path = model.export(format="tflite", int8=True)
+# path = model.export(format="tflite", int8=True)
+
+# Export in onnx format
+path = model.export(format="onnx")#, data="/mnt/disk1/datasets/iNaturalist/Arthropods/LIMIT18/dataset/Arthropods_wave18.yaml")
