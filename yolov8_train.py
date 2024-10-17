d1e02045a8d80b214b3f8fc3e9b4f0ece088fba6
edgaremy <remy.edgar@gmail.com>
2024-10-17 15:41:58 +0200

    Tried train with recent data and model weights

--- yolov8_train.py
+++ yolov8_train.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 # Load a model
 # model = YOLO("yolov8n.yaml")  # build a new model from scratch
 # model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
-model = YOLO('runs/detect/train16/weights/best.pt') # load best.pt or last.pt of local model
+model = YOLO('runs/detect/train40/weights/best.pt') # load best.pt or last.pt of local model
 
 
 # Use the model
@@ -11,6 +11,6 @@ model = YOLO('runs/detect/train16/weights/best.pt') # load best.pt or last.pt of
 
 # Evaluate model performance on the validation set
 # metrics = model.val()
-metrics = model.val(data="/mnt/disk1/datasets/Projet_Bees_Detection_Basile/data_bees_detection/BD307/DG_evaluate_predictions/dataset/DG_Detection.yaml")
+metrics = model.val(data="/mnt/disk1/datasets/iNaturalist/Arthropods/LIMIT18/dataset/Arthropods_wave18.yaml")
 
 #path = model.export(format="onnx")
