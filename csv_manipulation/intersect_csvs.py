b75b4c63d41bb312c2952b2c2e29cb0766da2bf8
edgaremy <remy.edgar@gmail.com>
2024-11-05 15:26:57 +0100

    Updated intersection script with new examples of usage

--- csv_manipulation/intersect_csvs.py
+++ csv_manipulation/intersect_csvs.py
@@ -7,7 +7,8 @@ def intersect_csvs(csv1_path, csv2_path, csv1_column, csv2_column, output_path):
     csv1_data = np.genfromtxt(csv1_path, delimiter=',', dtype=None, names=True, encoding=None)
 
     # Read the second CSV file
-    csv2_data = np.genfromtxt(csv2_path, delimiter=';', dtype=None, names=True, encoding=None)
+    # csv2_data = np.genfromtxt(csv2_path, delimiter=';', dtype=None, names=True, encoding=None)
+    csv2_data = np.genfromtxt(csv2_path, delimiter=',', dtype=None, names=True, encoding=None)
 
     print(csv1_data.dtype.names)
     print(csv2_data.dtype.names)
@@ -16,16 +17,13 @@ def intersect_csvs(csv1_path, csv2_path, csv1_column, csv2_column, output_path):
         writer = csv.writer(f, delimiter=',')
         writer.writerow(csv1_data.dtype.names)  # Write the header row
 
-    # Find the common values in the given columns
-    # i = 0
-    common_values = np.array([], dtype=csv1_data[csv1_column].dtype)
     for row1 in tqdm(csv1_data):
-        # i += 1
-        # if i == 50:
-        #     break
+        # # only start at a given row
+        # if row1["name"] <= '"Micrepeira tubulofaciens"':
+        #     continue
         for row2 in csv2_data:
-            if row1[csv1_column][1:-1] in row2[csv2_column][1:-1]:
-                # print(row1[csv1_column])
+            # if row1[csv1_column][1:-1] in row2[csv2_column][1:-1]:
+            if row1[csv1_column] == row2[csv2_column]:
                 with open(output_path, 'a') as f:
                     writer = csv.writer(f, delimiter=',', quotechar="'")
                     writer.writerow(row1)
@@ -33,10 +31,22 @@ def intersect_csvs(csv1_path, csv2_path, csv1_column, csv2_column, output_path):
 
 
 # Example usage
-csv1_path = 'requested_CSVs/all_arthropods_species.csv'
-csv2_path = '/home/eremy/Téléchargements/INPNrechercherrechercher_2024126.csv'
-csv1_column = 'name'
-csv2_column = 'Nom_valide'
-output_path = 'intersected_csv.csv'
+# csv1_path = 'requested_CSVs/all_arthropods_species.csv'
+# csv2_path = '/home/eremy/Téléchargements/INPNrechercherrechercher_2024126.csv'
+# csv1_column = 'name'
+# csv2_column = 'Nom_valide'
+# output_path = 'intersected_csv.csv'
+
+# csv1_path = 'requested_CSVs/all_arthropods_species.csv'
+# csv2_path = 'requested_CSVs/south_american_arthro/all_south_american_species.csv'
+# csv1_column = 'taxon_id'
+# csv2_column = 'taxon_id'
+# output_path = 'requested_CSVs/south_american_arthro/all_south_american_arthro_species_tmp.csv'
+
+csv1_path = 'requested_CSVs/south_american_arthro/all_south_american_arthro_species.csv'
+csv2_path = 'requested_CSVs/all_french_arthropods.csv'
+csv1_column = 'taxon_id'
+csv2_column = 'taxon_id'
+output_path = 'requested_CSVs/south_american_arthro/all_south_american_arthro_species_intersect_test.csv'
 
 intersect_csvs(csv1_path, csv2_path, csv1_column, csv2_column, output_path)
