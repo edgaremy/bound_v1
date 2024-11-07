ce430279d65faf8b9447a349e727f1132acefb3d
edgaremy <remy.edgar@gmail.com>
2024-11-07 11:45:52 +0100

    Display stats directly in terminal

--- dataset_analysis/get_dataset_bbox_stats.py
+++ dataset_analysis/get_dataset_bbox_stats.py
@@ -20,15 +20,19 @@ def get_bbox_stats(dataset_folder):
             for bbox in bboxes:
                 _, x_center, y_center, width, height = map(float, bbox.split())
                 bbox_area = width * height
-                total_bbox_area += bbox_area
+                total_bbox_area += bbox_area # bbox_area is already in percentage of the image area with yolo format
 
     mean_bbox_size = total_bbox_area / total_bboxes if total_bboxes else 0
     mean_bboxes_per_image = total_bboxes / total_images if total_images else 0
 
+    print(f"\nDataset: {dataset_folder}")
+    print(f"{total_bboxes} bounding boxes for {total_images} images")
+    print(f"Mean bounding box size: {mean_bbox_size:.2f}%")
+    print(f"Mean bounding box number per image: {mean_bboxes_per_image:.2f}")
+
     return mean_bbox_size, mean_bboxes_per_image
 
 # Example usage:
-dataset_folder = "/mnt/disk1/datasets/iNaturalist/Arthropods/dataset17"
-mean_bbox_size, mean_bboxes_per_image = get_bbox_stats(dataset_folder)
-print(f"Mean bounding box size: {mean_bbox_size:.2f}%")
-print(f"Mean number of bounding boxes per image: {mean_bboxes_per_image:.2f}")
+dataset_list = ["/mnt/disk1/datasets/iNaturalist/Arthropods/dataset17"]
+for dataset_folder in dataset_list:
+    get_bbox_stats(dataset_folder)
