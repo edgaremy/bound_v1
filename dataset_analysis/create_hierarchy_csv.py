3543d6611d762f7979d08909430bc5e0cab086bc
edgaremy <remy.edgar@gmail.com>
2024-11-29 10:53:51 +0100

    Added support for dataset with taxon_id as class names

--- dataset_analysis/create_hierarchy_csv.py
+++ dataset_analysis/create_hierarchy_csv.py
@@ -30,4 +30,38 @@ def create_hierarchy_csv(directory_path, output_csv):
         writer.writerows(hierarchy_data)
 
 # Example usage:
-create_hierarchy_csv('/mnt/disk1/datasets/Projet_Bees_Detection_Basile/data_bees_detection/BD307/BD_307_cropped/dataset/train', '306_hierarchy.csv')
+# create_hierarchy_csv('/mnt/disk1/datasets/Projet_Bees_Detection_Basile/data_bees_detection/BD307/BD_307_cropped/dataset/train', '306_hierarchy.csv')
+
+def create_hierarchy_csv_from_img_taxon_id(directory_path, output_csv):
+    # get all taxon_id from the images
+    taxon_ids = []
+    for image_name in os.listdir(directory_path):
+        taxon_id = image_name.split('_')[0]
+        if taxon_id not in taxon_ids:
+            taxon_ids.append(taxon_id)
+
+    hierarchy_data = []
+    
+    for taxon_id in taxon_ids:
+            hierarchy_list = hierarchy.get_hierarchy_from_taxon_id(taxon_id)
+            
+            if hierarchy_list:
+                hierarchy_data.append(hierarchy_list)
+    
+    # Sort the hierarchy_data list by the specie name (order of rows needs to match order of classes in the model)
+    hierarchy_data.sort(key=lambda x: x[4])
+    
+    # Check if the output_csv file exists
+    file_exists = os.path.isfile(output_csv)
+    
+    with open(output_csv, 'a', newline='') as file:
+        writer = csv.writer(file)
+        
+        # Write the header if the file doesn't exist
+        if not file_exists:
+            writer.writerow(['class', 'order', 'family', 'genus', 'specie'])
+        
+        writer.writerows(hierarchy_data)
+
+# Example usage:
+create_hierarchy_csv_from_img_taxon_id('/home/eremy/datasets/iNaturalist/Arthropods/dataset17/images/train', 'arthro_dataset_hierarchy.csv')
