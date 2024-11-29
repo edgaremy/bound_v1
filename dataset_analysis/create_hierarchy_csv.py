ea1895576afd9318e08b04b3d220bd46da87a757
edgaremy <remy.edgar@gmail.com>
2024-11-29 16:54:17 +0100

    Added option to include taxon_id column in output csv

--- dataset_analysis/create_hierarchy_csv.py
+++ dataset_analysis/create_hierarchy_csv.py
@@ -32,7 +32,7 @@ def create_hierarchy_csv(directory_path, output_csv):
 # Example usage:
 # create_hierarchy_csv('/mnt/disk1/datasets/Projet_Bees_Detection_Basile/data_bees_detection/BD307/BD_307_cropped/dataset/train', '306_hierarchy.csv')
 
-def create_hierarchy_csv_from_img_taxon_id(directory_path, output_csv):
+def create_hierarchy_csv_from_img_taxon_id(directory_path, output_csv, include_taxon_id=False):
     # get all taxon_id from the images
     taxon_ids = []
     for image_name in os.listdir(directory_path):
@@ -43,10 +43,12 @@ def create_hierarchy_csv_from_img_taxon_id(directory_path, output_csv):
     hierarchy_data = []
     
     for taxon_id in taxon_ids:
-            hierarchy_list = hierarchy.get_hierarchy_from_taxon_id(taxon_id)
-            
-            if hierarchy_list:
-                hierarchy_data.append(hierarchy_list)
+        hierarchy_list = hierarchy.get_hierarchy_from_taxon_id(taxon_id)
+        
+        if hierarchy_list:
+            if include_taxon_id:
+                hierarchy_list.append(taxon_id)
+            hierarchy_data.append(hierarchy_list)
     
     # Sort the hierarchy_data list by the specie name (order of rows needs to match order of classes in the model)
     hierarchy_data.sort(key=lambda x: x[4])
@@ -59,9 +61,12 @@ def create_hierarchy_csv_from_img_taxon_id(directory_path, output_csv):
         
         # Write the header if the file doesn't exist
         if not file_exists:
-            writer.writerow(['class', 'order', 'family', 'genus', 'specie'])
+            header = ['class', 'order', 'family', 'genus', 'specie']
+            if include_taxon_id:
+                header.append('taxon_id')
+            writer.writerow(header)
         
         writer.writerows(hierarchy_data)
 
 # Example usage:
-create_hierarchy_csv_from_img_taxon_id('/home/eremy/datasets/iNaturalist/Arthropods/dataset17/images/train', 'arthro_dataset_hierarchy.csv')
+create_hierarchy_csv_from_img_taxon_id('/home/eremy/datasets/iNaturalist/Arthropods/dataset22/images/train', 'arthro_dataset_hierarchy.csv', include_taxon_id=True)
